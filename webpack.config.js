926fecb21e9be46ee943716c67fd1df8acd5f7c1
shiqianghui <shiqianghui@sinochem.com>
2019-06-05 18:16:41 +0800

    less-loader

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,13 @@ module.exports = {
 		},
   module: {
 	  rules: [
+			{
+				test: /\.less$/,
+				use: [
+					'style-loader',
+					'less-loader'
+					]
+			},
 	    {
 			test: /\.css$/,
 			use: [
